c30189d206cb4031ac018a726d7ecfe5b35d7431
23f1002571 <132745713+23f1002571@users.noreply.github.com>
2025-08-11 16:04:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import os
 from flask import Flask, render_template
-from dotenv import load_dotenv
-
-load_dotenv()
+import config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', '12345678')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+app.config['SECRET_KEY'] = config.SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config.SQLALCHEMY_TRACK_MODIFICATIONS
 
 import routes
 import models
-if __name__=='__main__':
+
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
