aa012d231d22775d64c7ebe70f5741294cffe64a
23f1002571 <132745713+23f1002571@users.noreply.github.com>
2025-08-11 16:04:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-from dotenv import load_dotenv
-from app import app
 import os
+from dotenv import load_dotenv
+
 load_dotenv()
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
-app.config['SECRET_KEY'] = '12345678'
+
+SECRET_KEY = os.getenv('SECRET_KEY', '12345678')
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
